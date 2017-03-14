a03751229e1a01022bfbc46b3c1b78359d71e8fb
guolin.huang <guolin.huang@advantech.com.cn>
2017-03-14 12:47:42 +0800

    [yaoyao-faculty/task-9] update task 9

--- yaoyao-faculty/task-9/index.js
+++ yaoyao-faculty/task-9/index.js
@@ -2,22 +2,36 @@
 'use strict';
 
 
-function testHoldemLevel (pokers) {
-    const values = new Map();
-    const flush = {
-        is: true,
-        type: null,
-    };
+const POKERS_TOTAL_COUNT = 52;
+const POKERS_VALUES_COUNT = 13;
+const POKERS_ONE_HAND_COUNT = 5;
+const POKERS_ONE_HAND_LEVEL = [
+    'High card', // 0
+    'One pair',
+    'Two pair',
+    'Three of a kind', //3
+    'Straight',
+    'Flush',
+    'Full house',
+    'Four of a kind',
+    'Straight Flush', // 8
+];
 
-    for (let i = 0; i < 5; i++) {
-        let type = Math.ceil(pokers[i] / 13);
-        let value = pokers[i] % 13;
+class Holdem {
+    static computeLevel (pokers) {
+        const values = new Map();
+        const types = new Set(); 
 
-        if (flush.is) {
-            if (flush.type === null) flush.type = type;
-            else if (flush.type !== type) flush.is = false;
+        for (let i = 0; i < POKERS_ONE_HAND_COUNT; i++) {
+            let value = pokers[i] % POKERS_VALUES_TOTAL;
+            let type = Math.ceil(pokers[i] / POKERS_VALUES_TOTAL);
+
+            values.set(value, values.has(value) ? values.get(value) + 1 : 1);
+            types.add(type);
         }
+    }
 
+    static compare (pokers1, pokers2) {
 
     }
 }
