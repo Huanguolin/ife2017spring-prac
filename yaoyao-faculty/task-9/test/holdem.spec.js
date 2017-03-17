04281501fb05c417120095cedc8385d6f5c36e0f
guolin.huang <guolin.huang@advantech.com.cn>
2017-03-17 16:03:58 +0800

    [yaoyao-faculty/task-9] add unit test env

---
+++ yaoyao-faculty/task-9/test/holdem.spec.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+let expect = chai.expect;
+
+describe('test', function() {
+  it('1 plus 1 should be equal 2', function() {
+    expect(1 + 1).to.be.equal(2);
+  });
+});
