88c35422560e695fd4963ea7cb503c3549fcfa1f
guolin.huang <guolin.huang@advantech.com.cn>
2017-03-22 12:55:20 +0800

    [yaoyao-faculty/task-9] add test case for holem#compare

--- yaoyao-faculty/task-9/test/holdem.spec.js
+++ yaoyao-faculty/task-9/test/holdem.spec.js
@@ -3,50 +3,53 @@ import Holdem from '../holdem/holdem';
 
 let expect = chai.expect;
 const computeLevel = Holdem.computeLevel;
+const compare = Holdem.compare;
+
+// tools
+const generatePokers = function (valList, typeList, messIt = true) {
+    let ret = valList.map( (v, i) => { 
+        return { v, t: typeList[i] }; 
+    });
+    if (messIt) {
+        const tmp = ret;
+        ret = [];
+        do {
+            let i = Math.random() * (tmp.length - 1);
+            i = Math.floor(i);
+            ret.push(tmp[i]);
+            tmp.splice(i, 1);
+        } while (tmp.length);
+    }
+    return ret;
+};
+const pokersDeepEql = function (a, b) {
+    let res = true;
+
+    if (a.length === b.length) {
+        const len = a.length;            
+        for (let i = 0; i < len; i++) {
+            if (res) {
+                let vEql = a[i].v === b[i].v;
+                let tEql = a[i].t === b[i].t;
+                if (!tEql && 
+                    (a[i].t === '*' ||
+                        b[i].t === '*')) { // '*' is equal to any value 
+                    tEql = true;
+                }
+                res = vEql && tEql; 
+            } else {
+                break;
+            }
+        }
+    } else {
+        res = false;
+    }
+
+    return res;
+};
+
 
 describe('holdem/holdem#computeLevel', () => {
-    // tools
-    const createPokers = function (valList, typeList, messIt = true) {
-        let ret = valList.map( (v, i) => { 
-            return { v, t: typeList[i] }; 
-        });
-        if (messIt) {
-            const tmp = ret;
-            ret = [];
-            do {
-                let i = Math.random() * (tmp.length - 1);
-                i = Math.floor(i);
-                ret.push(tmp[i]);
-                tmp.splice(i, 1);
-            } while (tmp.length);
-        }
-        return ret;
-    };
-    const pokersDeepEql = function (a, b) {
-        let res = true;
-
-        if (a.length === b.length) {
-            const len = a.length;            
-            for (let i = 0; i < len; i++) {
-                if (res) {
-                    let vEql = a[i].v === b[i].v;
-                    let tEql = a[i].t === b[i].t;
-                    if (!tEql && 
-                        (a[i].t === '*' ||
-                         b[i].t === '*')) { // '*' is equal to any value 
-                        tEql = true;
-                    }
-                    res = vEql && tEql; 
-                } else {
-                    break;
-                }
-            }
-        } else {
-            res = false;
-        }
-
-        return res;
-    };
 
     const test_level_8 = [
         /* Straight Flush */
@@ -66,11 +69,11 @@ describe('holdem/holdem#computeLevel', () => {
         { args: [[6, 7, 8, 9, 10, 11, 12], [1, 1, 1, 1, 1, 1, 1]],  expected: [8, [8, 9, 10, 11, 12], [1, 1, 1, 1, 1]] },
         { args: [[0, 1, 2, 3, 10, 11, 12], [3, 3, 3, 3, 3, 3, 3]],  expected: [8, [12, 0, 1, 2, 3], [3, 3, 3, 3, 3]] },  
     ];
-    test_level_8.forEach(function(test) {
+    test_level_8.forEach((test) => {
         let desc = `test level 8, input ${JSON.stringify(test.args)}, expected ${JSON.stringify(test.expected)}`; 
         it(desc, () => {
-            let input = createPokers(test.args[0], test.args[1]);
-            let output = createPokers(test.expected[1], test.expected[2], false);
+            let input = generatePokers(test.args[0], test.args[1]);
+            let output = generatePokers(test.expected[1], test.expected[2], false);
             let res = computeLevel(input);
             //console.log(res);
             expect(res.level === test.expected[0]).to.be.true;
@@ -90,11 +93,11 @@ describe('holdem/holdem#computeLevel', () => {
         { args: [[0, 0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 1, 2]],     expected: [7, [0, 0, 0, 0, 3], ['*', '*', '*', '*', 2]] }, 
         { args: [[10, 10, 10, 10, 7, 7, 7], [1, 2, 3, 0, 0, 1, 2]], expected: [7, [10, 10, 10, 10, 7], ['*', '*', '*', '*', '*']] }, 
     ];
-    test_level_7.forEach(function(test) {
+    test_level_7.forEach((test) => {
         let desc = `test level 7, input ${JSON.stringify(test.args)}, expected ${JSON.stringify(test.expected)}`; 
         it(desc, () => {
-            let input = createPokers(test.args[0], test.args[1]);
-            let output = createPokers(test.expected[1], test.expected[2], false);
+            let input = generatePokers(test.args[0], test.args[1]);
+            let output = generatePokers(test.expected[1], test.expected[2], false);
             let res = computeLevel(input);
             //console.log(res);
             expect(res.level === test.expected[0]).to.be.true;
@@ -114,11 +117,11 @@ describe('holdem/holdem#computeLevel', () => {
         { args: [[0, 0, 0, 6, 6, 1, 1], [1, 2, 3, 0, 0, 1, 2]],     expected: [6, [0, 0, 0, 6, 6], ['*', '*', '*', '*', '*']] }, 
         { args: [[10, 10, 10, 11, 7, 7, 7], [1, 2, 3, 0, 0, 1, 2]], expected: [6, [10, 10, 10, 7, 7], ['*', '*', '*', '*', '*']] }, 
     ];
-    test_level_6.forEach(function(test) {
+    test_level_6.forEach((test) => {
         let desc = `test level 6, input ${JSON.stringify(test.args)}, expected ${JSON.stringify(test.expected)}`; 
         it(desc, () => {
-            let input = createPokers(test.args[0], test.args[1]);
-            let output = createPokers(test.expected[1], test.expected[2], false);
+            let input = generatePokers(test.args[0], test.args[1]);
+            let output = generatePokers(test.expected[1], test.expected[2], false);
             let res = computeLevel(input);
             //console.log(res);
             expect(res.level === test.expected[0]).to.be.true;
@@ -139,18 +142,17 @@ describe('holdem/holdem#computeLevel', () => {
         { args: [[12, 1, 6, 3, 9, 12, 12], [1, 1, 1, 1, 1, 3, 2]],  expected: [5, [1, 3, 6, 9, 12], [1, 1, 1, 1, 1]] },
         { args: [[0, 0, 6, 3, 9, 12, 12], [1, 3, 1, 1, 1, 3, 1]],   expected: [5, [0, 3, 6, 9, 12], [1, 1, 1, 1, 1]] },
     ];
-    test_level_5.forEach(function(test) {
+    test_level_5.forEach((test) => {
         let desc = `test level 5, input ${JSON.stringify(test.args)}, expected ${JSON.stringify(test.expected)}`; 
         it(desc, () => {
-            let input = createPokers(test.args[0], test.args[1]);
-            let output = createPokers(test.expected[1], test.expected[2], false);
+            let input = generatePokers(test.args[0], test.args[1]);
+            let output = generatePokers(test.expected[1], test.expected[2], false);
             let res = computeLevel(input);
             //console.log(res);
             expect(res.level === test.expected[0]).to.be.true;
             expect(pokersDeepEql(res.pokers, output.reverse())).to.be.true;
         });
     });
-
 
     const test_level_4 = [
         /* Straight */
@@ -171,11 +173,11 @@ describe('holdem/holdem#computeLevel', () => {
         { args: [[0, 1, 2, 3, 12, 12, 12], [3, 1, 1, 1, 3, 2, 1]],  expected: [4, [12, 0, 1, 2, 3], ['*', 3, 1, 1, 1]] },
         { args: [[1, 2, 2, 2, 3, 4, 5], [0, 1, 2, 3, 0, 0, 0]],     expected: [4, [1, 2, 3, 4, 5], [0, '*', 0, 0, 0]] },
     ];
-    test_level_4.forEach(function(test) {
+    test_level_4.forEach((test) => {
         let desc = `test level 4, input ${JSON.stringify(test.args)}, expected ${JSON.stringify(test.expected)}`; 
         it(desc, () => {
-            let input = createPokers(test.args[0], test.args[1]);
-            let output = createPokers(test.expected[1], test.expected[2], false);
+            let input = generatePokers(test.args[0], test.args[1]);
+            let output = generatePokers(test.expected[1], test.expected[2], false);
             let res = computeLevel(input);
             //console.log(res);
             expect(res.level === test.expected[0]).to.be.true;
@@ -195,11 +197,11 @@ describe('holdem/holdem#computeLevel', () => {
         { args: [[2, 2, 2, 5, 9, 11, 12], [1, 2, 3, 0, 0, 0, 0]], expected: [3, [2, 2, 2, 12, 11], ['*', '*', '*', 0, 0]] },
         { args: [[2, 2, 2, 3, 4, 5, 7], [1, 2, 3, 0, 0, 0, 0]], expected: [3, [2, 2, 2, 7, 5], ['*', '*', '*', 0, 0]] },
     ];
-    test_level_3.forEach(function(test) {
+    test_level_3.forEach((test) => {
         let desc = `test level 3, input ${JSON.stringify(test.args)}, expected ${JSON.stringify(test.expected)}`; 
         it(desc, () => {
-            let input = createPokers(test.args[0], test.args[1]);
-            let output = createPokers(test.expected[1], test.expected[2], false);
+            let input = generatePokers(test.args[0], test.args[1]);
+            let output = generatePokers(test.expected[1], test.expected[2], false);
             let res = computeLevel(input);
             //console.log(res);
             expect(res.level === test.expected[0]).to.be.true;
@@ -218,11 +220,11 @@ describe('holdem/holdem#computeLevel', () => {
         { args: [[2, 2, 3, 3, 4, 5, 9], [1, 2, 1, 2, 0, 0, 0]], expected: [2, [3, 3, 2, 2, 9], ['*', '*', '*', '*', 0]] },
         { args: [[2, 2, 3, 3, 4, 4, 7], [1, 2, 1, 2, 1, 2, 0]], expected: [2, [4, 4, 3, 3, 7], ['*', '*', '*', '*', 0]] },
     ];
-    test_level_2.forEach(function(test) {
+    test_level_2.forEach((test) => {
         let desc = `test level 2, input ${JSON.stringify(test.args)}, expected ${JSON.stringify(test.expected)}`; 
         it(desc, () => {
-            let input = createPokers(test.args[0], test.args[1]);
-            let output = createPokers(test.expected[1], test.expected[2], false);
+            let input = generatePokers(test.args[0], test.args[1]);
+            let output = generatePokers(test.expected[1], test.expected[2], false);
             let res = computeLevel(input);
             //console.log(res);
             expect(res.level === test.expected[0]).to.be.true;
@@ -239,11 +241,11 @@ describe('holdem/holdem#computeLevel', () => {
         // input len is 7
         { args: [[0, 0, 3, 4, 5, 11, 12], [1, 2, 0, 0, 0, 0, 3]], expected: [1, [0, 0, 12, 11, 5], ['*', '*', 3, 0, 0]] },
     ];
-    test_level_1.forEach(function(test) {
+    test_level_1.forEach((test) => {
         let desc = `test level 1, input ${JSON.stringify(test.args)}, expected ${JSON.stringify(test.expected)}`; 
         it(desc, () => {
-            let input = createPokers(test.args[0], test.args[1]);
-            let output = createPokers(test.expected[1], test.expected[2], false);
+            let input = generatePokers(test.args[0], test.args[1]);
+            let output = generatePokers(test.expected[1], test.expected[2], false);
             let res = computeLevel(input);
             //console.log(res);
             expect(res.level === test.expected[0]).to.be.true;
@@ -260,15 +262,31 @@ describe('holdem/holdem#computeLevel', () => {
         // input len is 7
         { args: [[0, 1, 3, 4, 5, 11, 12], [1, 2, 0, 0, 0, 0, 3]], expected: [0, [12, 11, 5, 4, 3], [3, 0, 0, 0, 0]] },
     ];
-    test_level_1.forEach(function(test) {
+    test_level_1.forEach((test) => {
         let desc = `test level 1, input ${JSON.stringify(test.args)}, expected ${JSON.stringify(test.expected)}`; 
         it(desc, () => {
-            let input = createPokers(test.args[0], test.args[1]);
-            let output = createPokers(test.expected[1], test.expected[2], false);
+            let input = generatePokers(test.args[0], test.args[1]);
+            let output = generatePokers(test.expected[1], test.expected[2], false);
             let res = computeLevel(input);
             //console.log(res);
             expect(res.level === test.expected[0]).to.be.true;
             expect(pokersDeepEql(res.pokers, output)).to.be.true;
         });
     });
+});
+
+describe('holdem/holdem#compare', () => {
+
+    it('level is different', () => {
+        let level_8 = computeLevel(generatePokers([2, 3, 4, 5, 6], [0, 0, 0, 0, 0]));
+        let level_7 = computeLevel(generatePokers([4, 4, 4, 4, 6], [0, 1, 2, 3, 0]));
+        let level_4 = computeLevel(generatePokers([2, 3, 4, 5, 6], [0, 0, 0, 0, 3]));
+        let level_0 = computeLevel(generatePokers([4, 7, 8, 10, 12], [0, 1, 2, 3, 0]));
+
+        expect(compare(level_8, level_7) > 0).to.be.true;
+        expect(compare(level_4, level_0) > 0).to.be.true;
+        expect(compare(level_4, level_8) < 0).to.be.true;
+        expect(compare(level_0, level_7) < 0).to.be.true;
+    }); 
+
 });
