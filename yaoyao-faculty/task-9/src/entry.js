5a45fecf79caa9d3355e8891d43228b4e1a43240
guolin.huang <guolin.huang@advantech.com.cn>
2017-03-23 12:58:07 +0800

    [yaoyao-faculty/task-9] add project config

---
+++ yaoyao-faculty/task-9/src/entry.js
@@ -0,0 +1,19 @@
+
+'use strict';
+
+
+const POKERS_TOTAL_COUNT = 52;
+const POKERS_VALUES_COUNT = 13;
+const POKERS_ONE_HAND_COUNT = 5;
+const POKERS_ONE_HAND_LEVEL = [
+    'High card', // 0
+    'One pair',
+    'Two pair',
+    'Three of a kind', //3
+    'Straight',
+    'Flush',
+    'Full house',
+    'Four of a kind',
+    'Straight Flush', // 8
+];
+
---
+++ yaoyao-faculty/task-9/webpack.config.js
@@ -0,0 +1,104 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/entry.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'bundle.min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+                        // the "scss" and "sass" values for the lang attribute to the right configs here.
+                        // other preprocessors should work out of the box, no loader config like this nessessary.
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+                    }
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style-loader!css-loader!sass-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+|\?\w)?$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+|#\w)$/,
+                loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
+        }
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: false,
+        host: '0.0.0.0',
+        port: 8082,
+        public: 'localhost:8082',
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map'
+};
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map';
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': { NODE_ENV: '"production"' }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ]);
+}
